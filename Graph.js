e2363cfb8470ed7d60ec253fc3df2a029fa65822
coreysnyder <corey.snyder@aver.io>
2018-12-03 18:05:23 -0500

    Implemented a Graph data structure

---
+++ Graph.js
@@ -0,0 +1,74 @@
+function createNode(key) {
+  const neighbors = [];
+
+  return {
+    key,
+    neighbors,
+    addNeighbor(node) {
+      neighbors.push(node);
+    }
+  };
+}
+
+function createGraph(directed = false) {
+  const nodes = [];
+  const edges = [];
+
+  return {
+    directed,
+    nodes,
+    edges,
+
+    addNode(key) {
+      nodes.push(createNode(key));
+    },
+
+    getNode(key) {
+      return nodes.find(node => node.key === key);
+    },
+
+    addEdge(node1Key, node2Key) {
+      const node1 = this.getNode(node1Key);
+      const node2 = this.getNode(node2Key);
+
+      node1.addNeighbor(node2);
+      edges.push(`${node1Key}${node2Key}`);
+
+      if (!directed) {
+        node2.addNeighbor(node1);
+      }
+    },
+
+    print() {
+      return nodes
+      .map(({ children, key }) => {
+        let result = `${key}`;
+
+        if (children.length) {
+          result += ` => ${children
+          .map(node => node.key)
+          .join(' ')}`;
+        }
+
+        return result;
+      })
+      .join('\n');
+    },
+  };
+}
+
+const graph = createGraph(true);
+
+graph.addNode('Corey');
+graph.addNode('Kayleigh');
+graph.addNode('Daisy');
+
+
+graph.addEdge('Kayleigh', 'Corey');
+graph.addEdge('Corey', 'Kayleigh');
+graph.addEdge('Kayleigh', 'Daisy');
+graph.addEdge('Corey', 'Daisy');
+graph.addEdge('Daisy', 'Kayleigh');
+
+
+graph.print();
