3d5c6d3b8196fee1bd7d021c9ead1df185242c38
coreysnyder <corey.snyder@aver.io>
2018-12-18 15:25:46 -0500

    Adds depth first search to graph

--- BreadthFirstSearch.js
+++ BreadthFirstSearch.js
@@ -64,25 +64,49 @@ function createGraph(directed = false) {
         return acc;
       });
 
-      const queue = createQueue();
-      queue.add(startingNode);
+      const queue = createQueue(); // Create a new Queue
+      queue.add(startingNode); // Add our starting node to kick us off
 
       while (!queue.isEmpty()) {
-        const currentNode = queue.dequeue();
+        // While our Queue is not empty
+        const currentNode = queue.dequeue(); // Pull out the next node in the queue
 
-        if (!visited[currentNode.key]) {
-          visitFn(currentNode);
+
+        if (!visited[currentNode.key]) { // If we haven't already crawled this node
+          visitFn(currentNode); // Run our visit function callback on given the node
           visited[currentNode.key] = true; // Set it to visited
         }
 
-        currentNode.neighbors.forEach((node => {
-          if (!visited[node.key]) {
-            queue.add(node);
+        // Now we need to queue up all of this nodes neighbors/edges to be crawled
+        currentNode.neighbors.forEach((node => { // Loop over all neighbor nodes
+          if (!visited[node.key]) { // If we haven't already crawled this node
+            queue.add(node); // Queue it up
           }
         }));
       }
+    },
+
+    depthFirstSearch(startingNodeKey, visitFn) {
+      const startingNode = this.getNode(startingNodeKey);
+      const visited = nodes.reduce((acc, cur) => {
+        acc[cur.key] = false;
+        return acc;
+      }, {})
+
+      function explore(node){
+
+        if (!visited[node.key]) { // If we haven't already crawled this node
+          visitFn(node); // Run our visit function callback on given the node
+          visited[node.key] = true; // Set it to visited
+
+          node.neighbors.forEach( (node) => {
+            explore(node);
+          });
+        }
+      }
+      explore(startingNode);
     }
-  }
+  };
 }
 
 
@@ -95,17 +119,29 @@ graph.addNode('c');
 graph.addNode('d');
 graph.addNode('e');
 graph.addNode('f');
+graph.addNode('g');
 
 const edges = [
   ['a', 'b'],
   ['a', 'e'],
   ['a', 'f'],
   ['b', 'd'],
+  ['b', 'g'],
   ['b', 'e'],
   ['c', 'b'],
   ['d', 'c'],
   ['d', 'e'],
 ];
+
+/*
+a
+b
+d
+c
+e
+
+
+ */
 
 
 edges.forEach( (nodes) => {
@@ -117,5 +153,10 @@ graph.print();
 
 console.log('###### Breadth First Search Result ######');
 graph.breadthFirstSearch('a', (node) => {
+  console.log(node.key);
+});
+
+console.log('###### Dept First Search Result ######');
+graph.depthFirstSearch('a', (node) => {
   console.log(node.key);
 });
